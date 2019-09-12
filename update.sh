eb78d1a1d4fb29b7b262e6b20cbe97dea80860ce
Romain <romain.lecat1@gmail.com>
2019-09-12 21:30:01 +0200

    Karabiner config

--- update.sh
+++ update.sh
@@ -5,3 +5,6 @@ cp ~/.ssh/config ./.ssh/config
 cp ~/.zshrc ./.zshrc
 cp ~/.tmux.conf ./.tmux.conf
 cp ~/.vimrc ./.vimrc
+
+mkdir -p .config/karabiner/assets/complex_modifications/
+cp ~/.config/karabiner/assets/complex_modifications/grankeys.json .config/karabiner/assets/complex_modifications/grankeys.json
