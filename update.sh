197330c843b845b70c5ffdb6b11acfd39b22a092
Romain Lecat <romain.lecat1@gmail.com>
2019-12-05 16:25:12 +0100

    Inverted update job

--- update.sh
+++ update.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-mkdir -p .ssh
-cp ~/.ssh/config ./.ssh/config
 
-cp ~/.zshrc ./.zshrc
-cp ~/.tmux.conf ./.tmux.conf
-cp ~/.vimrc ./.vimrc
-
-mkdir -p .config/karabiner/assets/complex_modifications/
-cp ~/.config/karabiner/assets/complex_modifications/grankeys.json .config/karabiner/assets/complex_modifications/grankeys.json
+rsync --exclude '.git' --exclude `basename $0` -ravI . ~/
